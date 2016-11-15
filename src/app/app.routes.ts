5067277af4c007ae91cc64ffa208342abef87d4f
Stefan <kh2ouija@yahoo.com>
2016-11-15 21:28:04 +0200

    missing pages redirect to 404

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,7 +7,8 @@ import { NotFoundComponent } from './not-found/not-found.component';
 const routes: Routes = [
   { path: '', component: CreatePollComponent, pathMatch: 'full' },
   { path: '404', component: NotFoundComponent },
-  { path: 'q/:hash', component: ShowPollComponent }  
+  { path: 'q/:hash', component: ShowPollComponent },
+  { path: '**', redirectTo: '404' }
 ];
 
 export const routing = RouterModule.forRoot(routes);
